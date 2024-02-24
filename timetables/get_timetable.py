53f396105f39f1cd79ce9b7b050bde75b5961c86
PremPremPremPrem <prem.sy1952@gmail.com>
2024-02-24 20:59:57 +0700

    me brain no working

--- timetables/get_timetable.py
+++ timetables/get_timetable.py
@@ -7,8 +7,9 @@ def in_box(l, t, r, b, x, y):
 
 def get_rooms(words):
     global timetables
+    timetables[f'{grade}/{class_no + 1}'] = [[], [], [], [], []]
     for day in range(5):
-        timetables[f'{grade}/{class_no + 1}'] = ["0"] * 10
+        timetables[f'{grade}/{class_no + 1}'][day] = ['0'] * 10
         for period in range(10):
             x = 130 + 68 * period
             y = 195 + 87 * day
@@ -16,12 +17,12 @@ def get_rooms(words):
                 boundaries, text = word[:4], word[4]
                 if in_box(*boundaries, x, y):
                     print(f'Day {day+1}. Period {period+1}: Room {text}')
-                    timetables[f'{grade}/{class_no + 1}'][period] = text
+                    timetables[f'{grade}/{class_no + 1}'][day][period] = text
                 # Double period
                 elif in_box(*boundaries, x + 34, y):
                     print(f'Day {day+1}. Period {period+1} - {period+2}: Room {text}')
-                    timetables[f'{grade}/{class_no + 1}'][period] = text
-                    timetables[f'{grade}/{class_no + 1}'][period+1] = text
+                    timetables[f'{grade}/{class_no + 1}'][day][period] = text
+                    timetables[f'{grade}/{class_no + 1}'][day][period+1] = text
 
 
 for grade in range(1, 7):
